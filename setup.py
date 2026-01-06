ea28ac96136649c50151a059809dcbda697c9770
Shubham Patil <shubhampatil.patil@gmail.com>
2026-01-06 17:09:34 +0530

    Bump version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='esp_config_diff',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'Click',
